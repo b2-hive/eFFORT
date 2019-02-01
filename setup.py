b8a4fd072a9384ac2cead805be21feffae61b82e
Markus Prim <markus.prim@kit.edu>
2019-02-01 14:45:58 +0100

    make installable package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='eFFORT',
+    version='v.0.1.0',
+    packages=['eFFORT'],
+    url='https://gitlab.ekp.kit.edu/mapr/eFFORT',
+    license='',
+    author='Markus Prim',
+    author_email='markus.prim@kit.edu',
+    description='A tool for convenient reweighting between different form factors of semileptonic B decays.'
+)
