20c0592de04a0e2f824cd531847fbb01783f86c2
Maximilian Welsch <mwelsch93@gmail.com>
2019-12-11 13:18:03 +0100

    add click to requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Markus Prim',
     author_email='markus.prim@kit.edu',
     description='A tool for convenient reweighting between different form factors of semileptonic B decays.',
-    install_requires=['numpy', 'scipy', 'matplotlib', 'tabulate', 'uncertainties', 'numdifftools'],
+    install_requires=['numpy', 'scipy', 'matplotlib', 'tabulate', 'uncertainties', 'numdifftools', 'click'],
     entry_points='''
         [console_scripts]
         download_btodstarstarlnu_data=eFFORT.SLBToC.utility:download_botdstarstarlnu_data
